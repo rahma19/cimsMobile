54aeec9264e84663af6e6600698cc4081aed3524
rahma19 <56127492+rahma19@users.noreply.github.com>
2021-03-29 19:48:42 +0200

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,17 +17,17 @@ const routes: Routes = [
         ]
       },
       {
-        path: 'home',
+        path: 'login',
         children: [
           {
             path: '',
-            loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+            loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
           },
         ]
       },
       {
         path: '',
-        redirectTo: 'home',
+        redirectTo: 'login',
         pathMatch: 'full'
       }
     ]
@@ -38,8 +38,8 @@ const routes: Routes = [
         pathMatch: 'full'
       },
   {
-        path: 'login',
-        loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+        path: 'home',
+        loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
     path: 'signup',
