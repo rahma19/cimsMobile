82d547d92603c928f4d14d6ce30cf360dd31fe27
rahma19 <56127492+rahma19@users.noreply.github.com>
2021-04-01 23:11:59 +0200

    heuheu

--- src/app/consultation/consultation.page.ts
+++ src/app/consultation/consultation.page.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./consultation.page.scss'],
 })
 export class ConsultationPage implements OnInit {
-
+test:boolean=true;
   constructor() { }
-
+affiche(){
+  this.test=false;
+}
   ngOnInit() {
+    this.test=true;
   }
 
 }
